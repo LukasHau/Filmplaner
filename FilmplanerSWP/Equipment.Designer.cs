6fbd5d365686111a09e3a9a6e27749732e3f1d38
LukasHau <lukas.hauser@hak-kitz.ac.at>
2021-11-04 09:04:20 +0100

    04-11-2021, 01

---
+++ FilmplanerSWP/Equipment.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace FilmplanerSWP
+{
+    partial class Equipment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cB_addEquipment = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cB_addEquipment
+            // 
+            this.cB_addEquipment.AutoSize = true;
+            this.cB_addEquipment.Location = new System.Drawing.Point(12, 12);
+            this.cB_addEquipment.Name = "cB_addEquipment";
+            this.cB_addEquipment.Size = new System.Drawing.Size(131, 17);
+            this.cB_addEquipment.TabIndex = 0;
+            this.cB_addEquipment.Text = "Equipment hinzufügen";
+            this.cB_addEquipment.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(599, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(599, 291);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(599, 240);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(189, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Equipment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cB_addEquipment);
+            this.Name = "Equipment";
+            this.Text = "Equipment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cB_addEquipment;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
