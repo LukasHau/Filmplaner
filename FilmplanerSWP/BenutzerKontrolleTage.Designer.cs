d7ec35aa6f2239230ed9835693cfc6657a8777bd
DavidZott <david.zott@hak-kitz.ac.at>
2021-12-16 09:02:52 +0100

    Kalender - BenutherKontrolleLeer - BenutherKontrolleTage Anfang mit Kalenderfunktion Erstellung von Benutzersteuerelementen

---
+++ FilmplanerSWP/BenutzerKontrolleTage.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace FilmplanerSWP
+{
+    partial class BenutzerKontrolleTage
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_days = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_days
+            // 
+            this.lbl_days.AutoSize = true;
+            this.lbl_days.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_days.Location = new System.Drawing.Point(42, 33);
+            this.lbl_days.Name = "lbl_days";
+            this.lbl_days.Size = new System.Drawing.Size(23, 19);
+            this.lbl_days.TabIndex = 0;
+            this.lbl_days.Text = "00";
+            // 
+            // BenutzerKontrolleTage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lbl_days);
+            this.Name = "BenutzerKontrolleTage";
+            this.Size = new System.Drawing.Size(107, 86);
+            this.Load += new System.EventHandler(this.BenutzerKontrolleTage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_days;
+    }
+}
---
+++ FilmplanerSWP/BenutzerKontrolleTage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilmplanerSWP
+{
+    public partial class BenutzerKontrolleTage : UserControl
+    {
+        public BenutzerKontrolleTage()
+        {
+            InitializeComponent();
+        }
+
+        private void BenutzerKontrolleTage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void days(int numday)
+        {
+            lbl_days.Text = numday + "";
+        }
+    }
+}
